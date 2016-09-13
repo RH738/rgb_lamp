81afed058769cff5dd92dbcfdd40edbbce76def2
rob738 <robertschvz@googlemail.com>
2016-09-13 21:39:44 +0200

    not sure what changed

--- public/schedule-list/schedule-list.component.js
+++ public/schedule-list/schedule-list.component.js
@@ -1,17 +1,14 @@
-    angular.
-    module('rgbLampApp', []).
-    component('scheduleList', {
-        templateUrl: 'schedule-list/schedule-list.template.html',
-        controller: function scheduleController($http) {
-            var ctrl = this;
-
-
-
-            $http.get('/:Entries').
-            then(function(res){
-                ctrl.entries = res.data;
-            }, function(res){
-                console.error("error from server = " + res);
-            });
-        }
-    })
+angular.
+module('rgbLampApp', []).
+component('scheduleList', {
+    templateUrl: 'schedule-list/schedule-list.template.html',
+    controller: function scheduleController($http) {
+        var ctrl = this;
+        $http.get('/:Entries').
+        then(function(res) {
+            ctrl.entries = res.data;
+        }, function(res) {
+            console.error("error from server = " + res);
+        });
+    }
+})
